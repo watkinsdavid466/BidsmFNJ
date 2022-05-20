320b7ea9286deba3e9b6ddccba593d392ea6ccad
watkinsdavid466 <104131845+watkinsdavid466@users.noreply.github.com>
2022-05-20 15:10:23 +0200

    Update opml2Text.py

--- opml2Text.py
+++ opml2Text.py
@@ -6,6 +6,5 @@ tree = ET.parse('RAW.opml')
 root = tree.getroot()
 if __name__ == '__main__':
     for child in root[1]:
-        print(child.attrib['title'])
         for child2 in child:
             print("\t"+child2.attrib['title']+': '+child2.attrib['xmlUrl'])
